97f65db0a346897cefabddd4957ae030eaec5f32
lucinda-aemilia <lucinda.aemilia@gmail.com>
2016-08-30 20:56:03 +0800

    打算写危险判断

--- board.cpp
+++ board.cpp
@@ -7,8 +7,21 @@ Board::Board(QWidget *parent) :
 {
     ui->setupUi(this);
     m_backgroundColor = QColor("tan");
+    m_margin = 60;
     // memset(m_inputArea, 0, sizeof(m_inputArea));
 
+    // 测试部分
+    init(Run, Qt::white);
+}
+
+void Board::init(const State &state, const QColor &pieceColor)
+{
+    m_state = state;
+    m_pieceColor = pieceColor;
+    m_otherPieceColor = pieceColor == Qt::white ? Qt::black : Qt::white;
+    for (int i = 0; i < 15; i++)
+        for (int j = 0; j < 15; j++)
+            m_board[i][j] = None;
 }
 
 Board::~Board()
@@ -16,20 +29,58 @@ Board::~Board()
     delete ui;
 }
 
+void Board::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton && m_state == Run)
+    {
+        int x = event->pos().x(), y = event->pos().y();
+        qDebug() << x << y;
+        int r = toIndex(x - (width()-m_length)/2);
+        int c = toIndex(y - (height()-m_length)/2);
+        if (m_board[r][c] != PieceType::None)
+            return;
+        qDebug() << r << c;
+        Piece piece(r, c, MyPiece);
+        addPiece(piece);
+        return;
+    }
+    QWidget::mouseMoveEvent(event);
+}
+
+void Board::addPiece(const Piece &piece)
+{
+    if (piece.row() < 0 || piece.column() < 0)
+        return;
+    for (int i = 0; i < m_pieces.size(); i++)
+    {
+        if (piece.row() == m_pieces.at(i).row() && piece.column() == m_pieces.at(i).column())
+            return;
+    }
+    m_board[piece.row()][piece.column()] = PieceType::MyPiece;
+    m_pieces.append(piece);
+    update();
+}
+
+int Board::toIndex(int pos)
+{
+    return std::floor(pos / (m_length/14.0) + 0.5);
+}
+
 // 绘制棋盘
 void Board::paintEvent(QPaintEvent *event)
 {
 
-
     // 画格子
     int totWidth = width(), totHeight = height();
-    int length = std::min(totWidth, totHeight) - 30;
+    int length = std::min(totWidth, totHeight) - m_margin;
+    m_length = length;
 
     m_pieceSize = length / 14.0 * 0.45;
     m_pointSize = std::floor(length / 14.0 * 0.05 + 0.5);
     m_pointSize = m_pointSize == 0 ? 1 : m_pointSize;
 
     QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing, true);
     painter.setPen(Qt::black);
     painter.translate((totWidth - length)/2, (totHeight - length)/2);
 
@@ -63,5 +114,50 @@ void Board::paintEvent(QPaintEvent *event)
             painter.restore();
         }
 
+    // 画棋子
+    for (int i = 0; i < m_pieces.size(); i++)
+    {
+        painter.save();
+        if (m_pieces.at(i).type() == MyPiece)
+            painter.setBrush(m_pieceColor);
+        else
+            painter.setBrush(m_otherPieceColor);
+        painter.setPen(Qt::black);
+        painter.translate(length / 14.0 * m_pieces.at(i).row(),
+                          length / 14.0 * m_pieces.at(i).column());
+        painter.drawEllipse(QPoint(0, 0), m_pieceSize, m_pieceSize);
+        painter.restore();
+    }
+
+    // 画危险位置
+
+
     event->accept();
+}
+
+void Board::calcDangerous()
+{
+    m_dangers.clear();
+
+    for (int i = 0; i < 15; i++)
+        for (int j = 0; j < 15; j++)
+        {
+            if (m_board[i][j] != PieceType::None)
+                continue;
+            m_pieces.append(Piece(i, j, PieceType::OtherPiece));
+            m_board[i][j] = PieceType::OtherPiece;
+
+            int three = 0;
+            int halfFour = 0;
+            calcDangerous(&three, &halfFour);
+
+            m_board[i][j] = PieceType::None;
+            m_pieces.removeLast();
+        }
+
+}
+
+void Board::calcDangerous(int* three, int* halfFour)
+{
+
 }
--- board.h
+++ board.h
@@ -9,6 +9,7 @@
 #include <QBrush>
 #include <QRect>
 #include <QVector>
+#include <QDebug>
 
 namespace Ui {
 class Board;
@@ -19,25 +20,63 @@ class Board : public QWidget
     Q_OBJECT
 
 public:
-    enum PieceType { MyPiece, OtherPiece };
+    enum PieceType { MyPiece, OtherPiece, None };
+    enum State { WaitForConnect, Pend, Run, Win, Lost };
     explicit Board(QWidget *parent = 0);
+    void init(const State& state, const QColor& pieceColor = Qt::white);
+    // 画棋盘和棋子
     void paintEvent(QPaintEvent* event);
+    void mousePressEvent(QMouseEvent* event);
+    // 检查是否下完后赢了
+    void checkState();
+    // 画出危险位置
+    void drawDangerous(bool danger)
+    {
+        m_danger = danger;
+        calcDangerous();
+        update();
+    }
+
     ~Board();
 
-protected:
     class Piece
     {
-        int row;
-        int column;
-        PieceType type;
+    public:
+        Piece(int r = 0, int c = 0, PieceType t = MyPiece) :
+            _row(r), _column(c), _type(t) {}
+        int row() const { return _row; }
+        int column() const { return _column; }
+        PieceType type() const { return _type; }
+
+    private:
+        int _row;
+        int _column;
+        PieceType _type;
     };
+
+protected:
+    void calcDangerous();
+    void calcDangerous(int* three, int* halfFour);
+
+public slots:
+    void addPiece(const Piece& piece);
+    int toIndex(int pos);
 
 private:
     Ui::Board *ui;
     QColor m_backgroundColor;
+    QColor m_pieceColor;
+    QColor m_otherPieceColor;
+    State m_state;
     int m_pointSize;
     int m_pieceSize;
-    QVector<Piece> pieces;
+    bool m_danger;
+
+    int m_margin;
+    int m_length;
+    QVector<Piece> m_pieces;
+    QVector<QPoint> m_dangers;
+    PieceType m_board[15][15];
 };
 
 #endif // BOARD_H
